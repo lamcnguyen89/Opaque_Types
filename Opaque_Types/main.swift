c36191d5f5617c7e6ca5c6b8ff3eb642c2866828
Lam.Nguyen <lam.nguyen@cognizant.com>
2022-01-02 18:43:20 -0500

    New

--- Opaque_Types/main.swift
+++ Opaque_Types/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Opaque Types
+// A function or method with an opaque return type hides its return value's type information.
+// Instead of providing a concrete type as the function's return type, the return value is described in terms of the protocols it supports.
 
+// Unlike returning a value whose type is a protocol type, opaque types preserve type identity- the compiler has access to the type information, but clients of the module don't.
+// Opaque types are like the opposite of a generic
+
+
+// MARK: - The Problem that Opaque Types Solve
+    // You don't want to expose detailed information about how a certain piece of code is executed so that competitors, hackers or whoever can steal data.
+
+// MARK: - Differences Between Opaque Types and Protocol Types
+// An opaque type refers to one specific type, although the caller of the function isn't able to see which type.
+// A protocol type can refer to any type that conforms to the protocol.
+// Protocol types give you more flexibility about the underlying types of the values they store while opaque types let you make stronger guarantees about those underlying types
